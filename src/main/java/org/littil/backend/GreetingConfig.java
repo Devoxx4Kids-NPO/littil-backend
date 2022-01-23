ee5b92fc365c2a901a24b024cdf1fdb38ff9d55f
Pepijn Schildkamp <pepijn.schildkamp@littil.org>
2022-01-23 17:41:00 +0100

    Added basic Quarkus scaffolding

---
+++ src/main/java/org/littil/backend/GreetingConfig.java
@@ -0,0 +1,12 @@
+package org.littil.backend;
+
+import io.smallrye.config.ConfigMapping;
+import io.smallrye.config.WithName;
+
+@ConfigMapping(prefix = "greeting")
+public interface GreetingConfig {
+
+    @WithName("message")
+    String message();
+
+}
---
+++ src/test/java/org/littil/backend/NativeReactiveGreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.littil.backend;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeReactiveGreetingResourceIT extends ReactiveGreetingResourceTest {
+    // Execute the same tests but in native mode.
+}
