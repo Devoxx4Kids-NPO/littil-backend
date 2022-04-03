8a9d7149e1a3ceb7041064d24127d958db6f26cc
jasper <jaspersprengers@outlook.com>
2022-04-03 12:25:49 +0200

    correct spelling of surname

--- src/main/java/org/littil/api/teacher/Teacher.java
+++ src/main/java/org/littil/api/teacher/Teacher.java
@@ -9,7 +9,7 @@ public class Teacher {
 
     private @Id @GeneratedValue Long id;
     private String firstName;
-    private String surName;
+    private String surname;
     private String email;
     private String postalCode;
     private String country;
@@ -18,10 +18,10 @@ public class Teacher {
 
     public Teacher () {}
 
-    public Teacher(String firstName, String surName,
+    public Teacher(String firstName, String surname,
                    String email, String postalCode) {
         this.firstName = firstName;
-        this.surName = surName;
+        this.surname = surname;
         this.email = email;
         this.postalCode = postalCode;
     }
@@ -34,12 +34,12 @@ public class Teacher {
         this.firstName = firstName;
     }
 
-    public String getSurName() {
-        return surName;
+    public String getSurname() {
+        return surname;
     }
 
-    public void setSurName(String surName) {
-        this.surName = surName;
+    public void setSurname(String surName) {
+        this.surname = surName;
     }
 
     public String getEmail() {
@@ -85,7 +85,7 @@ public class Teacher {
     @Override
     public String toString() {
         return "Teacher{" + "id=" + this.id + ", firstName='" + this.firstName + '\'' + ", surName='" +
-                this.surName + '\'' + ", email='" + this.email + '\'' + ", postalCode='" + this.postalCode +
+                this.surname + '\'' + ", email='" + this.email + '\'' + ", postalCode='" + this.postalCode +
                 ", country='" + this.country + '\'' + ", preferences='" + this.preferences + '\'' +
                 ", availability='" + this.availability + '\'' + '}';
     }
