3eb0c61a4025aa99f9654643b2d48e225d92722b
pschildkamp <pepijn.schildkamp@littil.org>
2022-06-17 20:58:23 +0200

    Implemented OIDC config / openapi spec enhancements.

--- src/main/java/org/littil/api/LittilBackendAPI.java
+++ src/main/java/org/littil/api/LittilBackendAPI.java
@@ -1,33 +1,38 @@
 package org.littil.api;
 
 
+import org.eclipse.microprofile.openapi.annotations.Components;
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
 import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.info.Contact;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
 import org.eclipse.microprofile.openapi.annotations.info.License;
-import org.eclipse.microprofile.openapi.annotations.security.*;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlow;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthFlows;
+import org.eclipse.microprofile.openapi.annotations.security.OAuthScope;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 
 import javax.ws.rs.core.Application;
 
-@SecuritySchemes( {
-        @SecurityScheme(
-                securitySchemeName = "security-oauth",
-                type = SecuritySchemeType.OAUTH2,
-                flows = @OAuthFlows(
-                        implicit = @OAuthFlow(
-                                authorizationUrl = "https://dev-g60bne29.eu.auth0.com/authorize",
-                                tokenUrl = "https://dev-g60bne29.eu.auth0.como/auth/token",
-                                scopes = {}
-                        )
-                )
-        )
-}
-)
 @OpenAPIDefinition(
-//        security = @SecurityRequirement(name = "security-oauth"),
+        components = @Components(
+                securitySchemes =
+                @SecurityScheme(
+                        securitySchemeName = "auth0",
+                        type = SecuritySchemeType.OAUTH2,
+                        flows = @OAuthFlows(
+                                implicit = @OAuthFlow(
+                                        authorizationUrl = "https://dev-g60bne29.eu.auth0.com/authorize",
+                                        scopes = {
+                                                @OAuthScope(name = "openid"),
+                                                @OAuthScope(name = "email"),
+                                                @OAuthScope(name = "profile")
+                                        }
+                                )
+                        )
+                )),
         info = @Info(
-                title="Littil backend API",
+                title = "Littil backend API",
                 version = "1.0.0",
                 contact = @Contact(
                         name = "Littil",
@@ -38,4 +43,6 @@ import javax.ws.rs.core.Application;
                         url = "https://www.apache.org/licenses/LICENSE-2.0.html"))
 )
 public class LittilBackendAPI extends Application {
+
+
 }
