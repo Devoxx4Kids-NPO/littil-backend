bed837f01709596997fbf7e21cb7e92348cd28e8
Wilko Zonnenberg <wilko.zonnenberg@crv4all.com>
2022-08-11 22:22:39 +0200

    Add mailing

--- src/main/java/org/littil/api/mail/MailService.java
+++ src/main/java/org/littil/api/mail/MailService.java
@@ -1,10 +1,24 @@
 package org.littil.api.mail;
 
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
 import org.littil.api.user.service.User;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+
+@ApplicationScoped
 public class MailService {
+
+    @Inject
+    Mailer mailer;
 
     public void sendWelcomeMail(User user, String password) {
         // todo do mail things
+        Mail mail = new Mail();
+        mail.addTo(user.getEmailAddress());
+        mail.setSubject("somesubject");
+
+        mailer.send(mail);
     }
 }
