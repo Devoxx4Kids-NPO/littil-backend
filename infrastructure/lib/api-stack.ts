016be573d6fdecf4c0dd2de8a3eb57ee943384ba
L. Gijsen <lennertgijsen@home.nl>
2022-10-31 00:22:12 +0100

    LITTIL-96 Allow fargate service access to database

--- infrastructure/lib/api-stack.ts
+++ infrastructure/lib/api-stack.ts
@@ -1,19 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
-import { CfnOutput, SecretValue } from 'aws-cdk-lib';
+import { CfnOutput } from 'aws-cdk-lib';
 import { Certificate } from 'aws-cdk-lib/aws-certificatemanager';
-import { InstanceClass, InstanceSize, InstanceType, Vpc } from 'aws-cdk-lib/aws-ec2';
+import { InstanceClass, InstanceSize, InstanceType, Peer, Port, SecurityGroup, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Repository } from 'aws-cdk-lib/aws-ecr';
 import { ContainerImage, Secret } from 'aws-cdk-lib/aws-ecs';
 import { ApplicationLoadBalancedFargateService } from 'aws-cdk-lib/aws-ecs-patterns';
-import {
-    Credentials,
-    DatabaseInstance,
-    DatabaseInstanceEngine,
-    MariaDbEngineVersion,
-} from 'aws-cdk-lib/aws-rds';
+import { Credentials, DatabaseInstance, DatabaseInstanceEngine, MariaDbEngineVersion, } from 'aws-cdk-lib/aws-rds';
 import { DatabaseInstanceProps } from 'aws-cdk-lib/aws-rds/lib/instance';
 import { Secret as SecretsManagerSecret } from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
+import { data } from "aws-cdk/lib/logging";
 
 export interface ApiStackProps extends cdk.StackProps {
     ecrRepository: Repository;
@@ -29,12 +25,11 @@ export class ApiStack extends cdk.Stack {
             natGateways: 1,
         });
 
+        /* Database. */
         const databaseName = 'LittilDatabase';
-        // TODO: Get from existing RDS secret
-        const databaseUsername = 'littil';
-        const databasePassword = 'littil-backend-staging';
 
-        const littilBackendDatabaseSecret = SecretsManagerSecret.fromSecretCompleteArn(this, 'LittilBackendDatabaseSecret', 'arn:aws:secretsmanager:eu-west-1:680278545709:secret:littil/backend/databaseCredentials-VuUWgu');
+        const littilDatabaseSecretName = 'littil/backend/databaseCredentials';
+        const littilBackendDatabaseSecret = SecretsManagerSecret.fromSecretNameV2(this, 'LittilBackendDatabaseSecret', littilDatabaseSecretName);
 
         const databaseProperties: DatabaseInstanceProps = {
             databaseName,
@@ -49,19 +44,19 @@ export class ApiStack extends cdk.Stack {
                 InstanceSize.MICRO,
             ),
         };
+
         const database = new DatabaseInstance(this, 'LittilApiDatabase', databaseProperties);
-
-        const littilBackendSecret = SecretsManagerSecret.fromSecretCompleteArn(this, 'LittilBackendSecret', 'arn:aws:secretsmanager:eu-west-1:680278545709:secret:littil/backend/staging-Muw9Id');
-
         new CfnOutput(this, 'dbEndpoint', {value: database.instanceEndpoint.hostname});
 
-        new ApplicationLoadBalancedFargateService(this, 'LittilApi', {
+        /* Fargate. */
+        const littilBackendSecret = SecretsManagerSecret.fromSecretCompleteArn(this, 'LittilBackendSecret', 'arn:aws:secretsmanager:eu-west-1:680278545709:secret:littil/backend/staging-Muw9Id');
+
+        const fargateService = new ApplicationLoadBalancedFargateService(this, 'LittilApi', {
             vpc,
             memoryLimitMiB: 512,
             desiredCount: 1,
             cpu: 256,
             taskImageOptions: {
-                // image: ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
                 image: ContainerImage.fromEcrRepository(props.ecrRepository, '0.0.1-SNAPSHOT-1'),
                 containerPort: 8080,
                 environment: {
@@ -84,5 +79,11 @@ export class ApiStack extends cdk.Stack {
             },
             certificate: props.certificate,
         });
+
+        const fargateSecurityGroup = fargateService.service.connections.securityGroups[0];
+
+        /* Database access. */
+        const databaseSecurityGroup = database.connections.securityGroups[0];
+        databaseSecurityGroup.connections.allowFrom(fargateSecurityGroup, Port.allTcp());
     }
 }
