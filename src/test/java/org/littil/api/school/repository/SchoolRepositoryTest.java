a1900bfc07e8c94bed2b991c27f1b4b4f77c741e
MarcelWildenburg <marcel.wildenburg.68@gmail.com>
2022-11-05 22:00:59 +0100

    LITTIL-167 test coverage

--- src/test/java/org/littil/api/school/repository/SchoolRepositoryTest.java
+++ src/test/java/org/littil/api/school/repository/SchoolRepositoryTest.java
@@ -1,18 +1,23 @@
 package org.littil.api.school.repository;
 
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.mockito.InjectSpy;
+import java.util.Optional;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
+import org.littil.api.location.repository.LocationEntity;
+import org.mockito.Mockito;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
 
 @QuarkusTest
 class SchoolRepositoryTest {
@@ -44,4 +49,22 @@ class SchoolRepositoryTest {
 
         assertThat(foundSchool).isEmpty();
     }
+
+    @Test
+    void givenFindByLocation_thenShouldReturnSuccessfully() {
+        String name = RandomStringUtils.randomAlphabetic(10);
+        Optional<SchoolEntity> school = Optional.of(new SchoolEntity(UUID.randomUUID(), name, null, null, null));
+
+        LocationEntity location = new LocationEntity();
+
+        PanacheQuery<SchoolEntity> query = Mockito.mock(PanacheQuery.class);
+        when(query.page(Mockito.any())).thenReturn(query);
+        when(query.firstResultOptional()).thenReturn(school);
+        when(repository.find("location", location)).thenReturn(query);
+
+        Optional<SchoolEntity> foundSchool = repository.findByLocation(location);
+
+        assertThat(school).isEqualTo(foundSchool);
+    }
+
 }
