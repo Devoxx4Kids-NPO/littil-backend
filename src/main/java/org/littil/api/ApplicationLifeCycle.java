725adf0f996a09e6bf6ffd38b99dc0fb550cdf73
Tubby <git@tubby.nl>
2023-01-14 14:22:56 +0100

    bootstrap dev-users using UserRepository

--- src/main/java/org/littil/api/ApplicationLifeCycle.java
+++ src/main/java/org/littil/api/ApplicationLifeCycle.java
@@ -9,18 +9,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.littil.api.auth.provider.Provider;
 import org.littil.api.auth.service.AuthorizationType;
+import org.littil.api.user.repository.UserEntity;
+import org.littil.api.user.repository.UserRepository;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
-import javax.persistence.PersistenceException;
-import javax.sql.DataSource;
 import javax.transaction.Transactional;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static java.util.Collections.emptyList;
 
@@ -41,13 +38,13 @@ public class ApplicationLifeCycle {
     String authorizationsClaimName;
 
     @Inject
-    DataSource dataSource;
+    UserRepository userRepository;
 
     @Inject
     ManagementAPI managementAPI;
 
     void onStart(@Observes StartupEvent ev) throws Auth0Exception {
-        log.info("The application is starting with profile " + ProfileManager.getActiveProfile());
+        log.info("The application is starting with profile {}",ProfileManager.getActiveProfile());
 
         if (insertDevData) {
             persistDevData();
@@ -72,34 +69,25 @@ public class ApplicationLifeCycle {
     @Transactional(Transactional.TxType.REQUIRES_NEW)
     void persistDevData() {
         log.info("Persisting development data to datasource, this should not be happening in staging nor production.");
+        devUsers()
+                .peek(this.userRepository::persist)
+                .map(UserEntity::getEmailAddress)
+                .forEach(email -> log.info("Created {} user for development purposes",email));
+        log.info("Added general LiTTiL users for development purposes. You can login via email-addresses and the default password to the dev tenant.");
+    }
 
-        try (Connection connection = dataSource.getConnection()) {
-            try (Statement stmt = connection.createStatement()) {
-                stmt.execute(String.format("""
-                        INSERT INTO `user` (
-                            user_id,
-                            provider,
-                            provider_id,
-                            email_address
-                        ) VALUES (
-                            %s,
-                            %s,
-                            %s,
-                            %s
-                        )""", "X'e87cda489a9841a789bb9ee62441c84c'", "'" + Provider.AUTH0.name() + "'", "'auth0|62fd3224f76949850e9eb264'", "'info@littil.org'"));
+    private Stream<UserEntity> devUsers() {
+        return Stream.of(
+                create("62fd3224f76949850e9eb264","info@littil.org")
+        );
+    }
 
-                ResultSet rs = stmt.executeQuery("SELECT COUNT(*) AS rowcount FROM `user`");
-                rs.next();
-
-                log.info("Created " + rs.getInt("rowcount") + " users for development purposes");
-
-            } catch (SQLException e) {
-                throw new PersistenceException("Unable to persist development data", e);
-            }
-        } catch (SQLException e) {
-            throw new PersistenceException("Unable to persist development data", e);
-        }
-
-        log.info(String.format("Added general LiTTiL user for development purposes. You can login via email-address %s and the default password to the dev tenant.", "'info@littil.org'"));
+    private UserEntity create(String auth0Id, String email) {
+        var user = new UserEntity();
+        //user.setId(UUID.from(e87cda48-9a98-41a7-89bb-9ee62441c84c)) // old fixed id
+        user.setProvider(Provider.AUTH0);
+        user.setProviderId("auth0|"+auth0Id);
+        user.setEmailAddress(email);
+        return user;
     }
 }
