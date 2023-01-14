72b6d45b9d9a37d78a7d6cc9f0f5a9a40a771888
Tubby <git@tubby.nl>
2023-01-14 15:32:52 +0100

    added docent1-3 and school1-3 dev users (and id’s)

--- src/main/java/org/littil/api/ApplicationLifeCycle.java
+++ src/main/java/org/littil/api/ApplicationLifeCycle.java
@@ -79,7 +79,13 @@ public class ApplicationLifeCycle {
 
     private Stream<UserEntity> devUsers() {
         return Stream.of(
-                create(UUID.fromString("e87cda48-9a98-41a7-89bb-9ee62441c84c"),"62fd3224f76949850e9eb264","info@littil.org")
+                create(UUID.fromString("e87cda48-9a98-41a7-89bb-9ee62441c84c"),"62fd3224f76949850e9eb264","info@littil.org"),
+                create(UUID.fromString("3fc95e78-b498-4d20-bc60-c6a672b8931f"),"63c2ba7ac4d4beeea5ad3d4e","testdocent1@littil.org"),
+                create(UUID.fromString("312badca-44c4-499b-a92a-df1c16b30877"),"63c2baa6efc9a808a903f2e9","testdocent2@littil.org"),
+                create(UUID.fromString("e7164fe3-52b3-4ac5-a2bf-f6ce085bea38"),"63c2bab9b4c082fb977f270e","testdocent3@littil.org"),
+                create(UUID.fromString("50e2b86b-51ed-4611-936b-d16adcb0b104"),"63c2baca2333c1e8b1545a54","testschool1@littil.org"),
+                create(UUID.fromString("72335590-78c0-4810-b793-cce4e22eff9a"),"63c2bad91fa0489a010f1ba3","testschool2@littil.org"),
+                create(UUID.fromString("6e0d0eb8-e187-4bde-a4f3-86a97fc66f65"),"63c2baf02333c1e8b1545a5a","testschool3@littil.org")
         );
     }
 
