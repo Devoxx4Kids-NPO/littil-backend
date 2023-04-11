e5bdf0c7e90d99c72d2ef610d24494609a4bcef3
Tubby <git@tubby.nl>
2023-04-11 20:35:19 +0200

    LITTIL-217 also for ConstraintViolationExceptionMapper, custom build for ErrorResponse

--- src/main/java/org/littil/api/exception/ConstraintViolationExceptionMapper.java
+++ src/main/java/org/littil/api/exception/ConstraintViolationExceptionMapper.java
@@ -2,19 +2,21 @@ package org.littil.api.exception;
 
 import javax.validation.ConstraintViolationException;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 import java.util.List;
 
 @Provider
-public class ConstraintViolationExceptionMapper implements ExceptionMapper<ConstraintViolationException> {
+public class ConstraintViolationExceptionMapper extends AbstractExceptionMapper<ConstraintViolationException> {
+
+    public ConstraintViolationExceptionMapper() {
+        super(Response.Status.BAD_REQUEST);
+    }
 
     @Override
-    public Response toResponse(ConstraintViolationException e) {
+    protected ErrorResponse build(ConstraintViolationException e) {
         List<ErrorResponse.ErrorMessage> errorMessages = e.getConstraintViolations().stream()
                 .map(constraintViolation -> new ErrorResponse.ErrorMessage(constraintViolation.getPropertyPath().toString(), constraintViolation.getMessage()))
                 .toList();
-        return Response.status(Response.Status.BAD_REQUEST).entity(new ErrorResponse(errorMessages)).build();
+        return new ErrorResponse(errorMessages);
     }
-
 }
