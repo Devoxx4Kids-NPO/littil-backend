01ca2c95bed4967884296a5aae829a727d6ec33b
Tubby <git@tubby.nl>
2023-04-11 20:33:40 +0200

    LITTIL-217 moved other ExceptionMappers under AbstractExceptionMapper

--- src/main/java/org/littil/api/exception/AuthProviderExceptionMapper.java
+++ src/main/java/org/littil/api/exception/AuthProviderExceptionMapper.java
@@ -3,7 +3,6 @@ package org.littil.api.exception;
 import org.littil.api.auth.provider.AuthProviderException;
 
 import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
 @Provider
