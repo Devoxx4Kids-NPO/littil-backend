8c9143698bfd79f384ed1f684bca298b0b9b6971
Tubby <git@tubby.nl>
2023-06-25 15:08:08 +0200

    alle relevante velden vooraan

--- src/main/resources/db/migration/V9_0_0__Create_Contact_Entity.sql
+++ src/main/resources/db/migration/V9_0_0__Create_Contact_Entity.sql
@@ -1,11 +1,11 @@
 CREATE TABLE contact
 (
     id                 BINARY(16)   NOT NULL,
+    recipient_id       BINARY(16)   NOT NULL,
     created_by         BINARY(16),
     created_date       DATETIME,
     last_modified_by   BINARY(16),
-    last_modified_date DATETIME,
-    recipient_id       BINARY(16)   NOT NULL,
+    last_modified_date DATETIME
     PRIMARY KEY (id),
     CONSTRAINT fk_contact_recipient
         FOREIGN KEY (recipient_id)
