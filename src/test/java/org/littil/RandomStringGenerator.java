174f833a1cd34bb616d90fa84e6ada2e9ca8b790
MarcelWildenburg <marcel.wildenburg.68@gmail.com>
2025-02-25 00:12:55 +0100

    fix sonar issues

--- src/test/java/org/littil/RandomStringGenerator.java
+++ src/test/java/org/littil/RandomStringGenerator.java
@@ -5,7 +5,7 @@ public class RandomStringGenerator {
     private static final org.apache.commons.text.RandomStringGenerator generator =
             new org.apache.commons.text.RandomStringGenerator.Builder() //
                     .withinRange('a', 'z') //
-                    .build();
+                    .get();
 
     public static String generate( int length) {
         return generator.generate(length);
